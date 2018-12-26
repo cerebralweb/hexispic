52aef358397c164ef4d966a40a24198f0490e318
cerebralweb <paulocleon@gmail.com>
2018-12-26 15:36:43 -0500

    JS drill-0

--- js/1.js
+++ js/1.js
@@ -9,9 +9,26 @@ total = illustration * cantidad;
 var x = document.getElementById("cost");
 x.textContent = "Ten illustrations worth " + '$' + total;
 
-var colores = ['blanco', 'negro', '#FFF'];
+var colores = ['blanco', 'azure', '#FFF'];
+
+colores[3] = 'negro';
 
 var m = document.getElementById('colorBanner');
-m.textContent = "soy una palabra de color " + colores[2];
+m.textContent = "soy una palabra de color " + colores[3];
 
+var name = "Bernardo";
+var sayHello = "hola";
+var hola = name + sayHello;
+
+var saludar = document.getElementById('saludo');
+saludar.textContent = hola;
+
+
+
+var costoIlustracion = 5;
+var cantidadIlustracionesSitioTipoOriginal = 8;
+var total = costoIlustracion * cantidadIlustracionesSitioTipoOriginal;
+
+var costoParcialIlustraciones = document.getElementById ('sitioTipoOriginalConIlustraciones');
+costoParcialIlustraciones.textContent = total;
 
