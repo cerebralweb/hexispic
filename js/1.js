e3899930b27fcdfb9f2f9d13183f9f3c7c174082
cerebralweb <paulocleon@gmail.com>
2018-12-27 13:45:44 -0500

    tabla costos

--- js/1.js
+++ js/1.js
@@ -10,6 +10,8 @@ nodojavascript = 70;
 cantidad = 0;
 subtotalPlantilla = 60;
 subTotalDesign = 100;
+esquema = 10;
+foto = 2; 
 hybridapp = 50;
 
 total = (illustration * cantidad) + subtotalPlantilla;
@@ -18,13 +20,13 @@ var s1Plantilla = document.getElementById("costS1");
 s1Plantilla.textContent = total;
 
 var s1Original = document.getElementById("costS1original");
-s1Original.textContent = subTotalDesign + ((jsRedes * 5) + (componentecss * 1) + (illustration * 1));
+s1Original.textContent = subTotalDesign +  ((jsRedes * 5) + (componentecss * 1) + (illustration * 1) + (foto * 1) + esquema);
 
 var s2 = document.getElementById("costoS2");
-s2.textContent = subTotalDesign + ((illustration * 5) + (componentecss * 3) + (jquery))
+s2.textContent = subTotalDesign + ((illustration * 5) + (componentecss * 3) + (jquery)  + (foto * 10) + esquema)
 
 var s3 = document.getElementById("costoS3");
-s3.textContent = subTotalDesign + ((illustration * 10) + (componentecss = 5) + (nodojavascript));
+s3.textContent = subTotalDesign + ((illustration * 10) + (componentecss = 5) + (nodojavascript) + (foto * 20) + esquema);
 
 
 var s4 = document.getElementById("costoS4");
